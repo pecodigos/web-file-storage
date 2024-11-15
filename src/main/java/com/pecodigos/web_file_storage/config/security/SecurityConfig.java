f8bf58c5a1afc905620e30a2027ae0d8aa50758e
pecodigos <pedrenriquecunha@gmail.com>
2024-11-15 05:34:26 -0300

    Implementing CORS config

--- src/main/java/com/pecodigos/web_file_storage/config/security/SecurityConfig.java
+++ src/main/java/com/pecodigos/web_file_storage/config/security/SecurityConfig.java
@@ -1,9 +1,9 @@
 package com.pecodigos.web_file_storage.config.security;
 
 import com.pecodigos.web_file_storage.auth.JwtAuthenticationFilter;
-import com.pecodigos.web_file_storage.auth.JwtUtil;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,15 +15,22 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final RetrieveUserService retrieveUserService;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final JwtUtil jwtUtil;
+
+    @Value("${cors.allowed.origins}")
+    private String allowedOrigins;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -37,17 +44,32 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/css/**", "/js/**", "/index.html", "/assets/**",
-                                "/", "/login.html", "/register.html", "/api/auth/login", "/api/auth/register", "/storage.html").permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers("/api/files/**").authenticated()
+                        .anyRequest().permitAll()
                 )
+                .userDetailsService(retrieveUserService)
                 .exceptionHandling(exception -> exception.authenticationEntryPoint((request, response, accessDeniedException) -> response.setStatus(HttpServletResponse.SC_UNAUTHORIZED)))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        var configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",\\s*")));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
